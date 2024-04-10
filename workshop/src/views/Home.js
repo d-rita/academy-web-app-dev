9fc898751f5a19e0f24e3030792a2fc1b487c1cb
Diana Nanyanzi <drnanyanzi@gmail.com>
2024-04-10 15:09:29 +0300

    feat: add i18n functionality to allow translations

--- workshop/src/views/Home.js
+++ workshop/src/views/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import i18n from '../locales/index.js'
 
 export const Home = () => (
     <div>
-        <h1>Home</h1>
+        <h1>{i18n.t('Home')}</h1>
 
-        <p>DHIS2 Web App Academy 2024</p>
+        <p>{i18n.t('DHIS2 Web App Academy 2024')}</p>
     </div>
 )
